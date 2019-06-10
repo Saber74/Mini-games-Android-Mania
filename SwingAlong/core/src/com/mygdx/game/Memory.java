8f35752c205b9cfb8bfa1d13b7a31ba35a050890
Nizar Alrifai <nizar.rifai7401@gmail.com>
2019-06-09 23:14:05 -0400

    edit

--- SwingAlong/core/src/com/mygdx/game/Memory.java
+++ SwingAlong/core/src/com/mygdx/game/Memory.java
@@ -19,7 +19,7 @@ import java.util.TimerTask;
 public class Memory extends ScreenAdapter {
     float stateTime;
     int time;
-
+    Boolean gamefinished=false;
     Boolean gameStart=true;
     MyGdxGame game;
     ShapeRenderer shapes=new ShapeRenderer();
@@ -70,33 +70,44 @@ public class Memory extends ScreenAdapter {
                     gameStart=false;
                 }
             };
-            new Timer().scheduleAtFixedRate(task, 10000, 1);
+            new Timer().scheduleAtFixedRate(task, 10000, 10000);
          }
-        else{
-            game.font.draw(game.batch,"Now Recreate!",550,400);
-            for (int i = 0; i < 7; i++) {
-                for (int k = 0; k < 7; k++) {
-                    if (player1[i][k] == 1) {
-                        shapes.setColor(0.2f, 0.5f, 0.1f, 0);
-                        shapes.rect(200 + (50 * k), 225 + 50 * i, 48, 48);
+        else {
+            if (!gamefinished) {
+                game.font.draw(game.batch, "Now Recreate!", 550, 400);
+                for (int i = 0; i < 7; i++) {
+                    for (int k = 0; k < 7; k++) {
+                        if (player1[i][k] == 1) {
+                            shapes.setColor(0.2f, 0.5f, 0.1f, 0);
+                            shapes.rect(200 + (50 * k), 225 + 50 * i, 48, 48);
+                        } else {
+                            shapes.setColor(255, 255, 255, 0);
+                            shapes.rect(200 + (50 * k), 225 + 50 * i, 48, 48);
+                        }
+                        if (player2[i][k] == 1) {
+                            shapes.setColor(0.2f, 0.5f, 0.1f, 0);
+                            shapes.rect(800 + (50 * k), 225 + 50 * i, 48, 48);
+                        } else {
+                            shapes.setColor(255, 255, 255, 0);
+                            shapes.rect(800 + (50 * k), 225 + 50 * i, 48, 48);
+                        }
                     }
-                    else {
-                        shapes.setColor(255, 255, 255, 0);
-                        shapes.rect(200 + (50 * k), 225 + 50 * i, 48, 48);
-                    }
-                    if(player2[i][k]==1){
-                        shapes.sedatColor(0.2f, 0.5f, 0.1f, 0);
-                        shapes.rect(800 + (50 * k), 225 + 50 * i, 48, 48);
-                    }
-                    else {
-                        shapes.setColor(255, 255, 255, 0);
-                        shapes.rect(800 + (50 * k), 225 + 50 * i, 48, 48);
-                    }
+
                 }
-
+                shapes.setColor(0.3f, 0.1f, 0.1f, 0);
+                shapes.rect(200 + (50 * currentp1x), 225 + 50 * currentp1y, 28, 28);
+                shapes.rect(800 + (50 * currentp2x), 225 + 50 * currentp2y, 28, 28);
+                TimerTask task=new TimerTask() {
+                    @Override
+                    public void run() {
+                        gamefinished=true;
+                    }
+                };
+                new Timer().scheduleAtFixedRate(task, 2000, 10000);
             }
-
-
+            else{
+                winnner();
+            }
         }
         shapes.end();
         game.batch.end();
@@ -107,7 +118,7 @@ public class Memory extends ScreenAdapter {
             @Override
             public boolean keyDown(int keyCode) {
                 if (keyCode == Input.Keys.RIGHT) {
-                    if (currentp1x < 7) {
+                    if (currentp1x < 6) {
                         currentp1x += 1;
                     }
                 }
@@ -117,7 +128,7 @@ public class Memory extends ScreenAdapter {
                     }
                 }
                 if (keyCode == Input.Keys.UP) {
-                    if (currentp1y < 7) {
+                    if (currentp1y < 6) {
                         currentp1y++;
                     }
                 }
@@ -139,12 +150,12 @@ public class Memory extends ScreenAdapter {
                     }
                 }
                 if (keyCode == Input.Keys.D) {
-                    if (currentp2x < 7) {
+                    if (currentp2x < 6) {
                         currentp2x += 1;
                     }
                 }
                 if (keyCode == Input.Keys.W) {
-                    if (currentp2y < 7) {
+                    if (currentp2y < 6) {
                         currentp2y++;
                     }
                 }
@@ -169,6 +180,57 @@ public class Memory extends ScreenAdapter {
     @Override
     public void hide() {
         Gdx.input.setInputProcessor(null);
+    }
+    public void winnner(){
+        int p1Score=0;
+        int p2Score=0;
+        for(int i=0;i<7;i++){
+            for(int k=0;k<7;k++){
+                if(memorize[i][k]==player1[i][k]){
+                    p1Score++;
+                }
+                if(memorize[i][k]==player2[i][k]){
+                    p2Score++;
+                }
+            }
+        }
+        Gdx.gl.glClearColor(.4f, .25f, 0.3f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.font.getData().setScale(1f);
+        for (int i = 0; i < 7; i++) {
+            for (int k = 0; k < 7; k++) {
+                if (player1[i][k] == 1) {
+                    shapes.setColor(0.2f, 0.5f, 0.1f, 0);
+                    shapes.rect(100 + (50 * k), 50 + 50 * i, 48, 48);
+                } else {
+                    shapes.setColor(255, 255, 255, 0);
+                    shapes.rect(100 + (50 * k), 50 + 50 * i, 48, 48);
+                }
+                if (player2[i][k] == 1) {
+                    shapes.setColor(0.2f, 0.5f, 0.1f, 0);
+                    shapes.rect(900 + (50 * k), 50 + 50 * i, 48, 48);
+                } else {
+                    shapes.setColor(255, 255, 255, 0);
+                    shapes.rect(900 + (50 * k), 50 + 50 * i, 48, 48);
+                }
+                if (memorize[i][k] == 1) {
+                    shapes.setColor(0.2f, 0.5f, 0.1f, 0);
+                    shapes.rect(475 + (50 * k), 300 + 50 * i, 48, 48);
+                } else {
+                    shapes.setColor(255, 255, 255, 0);
+                    shapes.rect(475 + (50 * k), 300 + 50 * i, 48, 48);
+                }
+            }
+        }
+        if(p1Score>p2Score){
+            game.font.draw(game.batch,"Player one won",575,1000);
+        }
+        else if (p1Score<p2Score){
+            game.font.draw(game.batch,"Player two won",575,1000);
+        }
+        else{
+            game.font.draw(game.batch,"It's a draw",575,1000);
+        }
     }
 }
 
