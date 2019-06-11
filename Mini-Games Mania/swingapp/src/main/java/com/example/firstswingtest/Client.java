4665ac269b90e5e6e6ce7b97b7917dd6b395c852
Nizar Alrifai <nizar.rifai7401@gmail.com>
2019-06-11 19:51:13 -0400

    a

--- Mini-Games Mania/swingapp/src/main/java/com/example/firstswingtest/Client.java
+++ Mini-Games Mania/swingapp/src/main/java/com/example/firstswingtest/Client.java
@@ -1,23 +1,38 @@
-//package com.example.firstswingtest;
-//import java.io.*;
-//import java.net.*;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.io.PrintStream;
+//import java.net.ServerSocket;
+//import java.net.Socket;
 //public class Client {
-//    public Socket sk;
-//    public PrintStream sout;
-//    public BufferedReader stdin;
-//    public Client(){
-//        try {
-//            sk = new Socket("127.0.0.1", 5014);
-//            sout = new PrintStream(sk.getOutputStream());
-//            stdin = new BufferedReader(new InputStreamReader(System.in));
+//
+//    public static void main(String args[]) throws Exception
+//    {
+//        Socket sk=new Socket("127.0.0.1",5013);
+//        BufferedReader sin=new BufferedReader(new InputStreamReader(sk.getInputStream()));
+//        PrintStream sout=new PrintStream(sk.getOutputStream());
+//        BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+//        String s;
+//        while (  true )
+//        {
+//            System.out.print("Client : ");
+//            s=stdin.readLine();
+//            sout.println(s);
+//            if ( s.equalsIgnoreCase("BYE") )
+//            {
+//                System.out.println("Connection ended by client");
+//                break;
+//            }
+//            s=sin.readLine();
+//            System.out.print("Server : "+s+"\n");
+//
 //        }
-//        catch (java.net.UnknownHostException x) {
-//            System.out.println("Unkown host exception");
-//        }
-//        catch(java.io.IOException y){
-//            System.out.println("IO exception");
-//        }
+//        sk.close();
+//        System.out.println("hi");
+//        sin.close();
+//        sout.close();
+//        stdin.close();
 //    }
+//
 //}
 package com.example.firstswingtest;
 import java.io.BufferedReader;
